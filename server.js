4b139fa2fe8746dfd696a40da055dc040b7f9553
landeilsonveloso <landeilsonveloso@gmail.com>
2025-07-30 11:34:49 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 import app from "./src/app.js"
 import envConfig from "./src/config/config.js"
 
-app.listen(envConfig.PORT, () => console.log("Servidor em execução..."))
+app.listen(envConfig.PORT, () => console.log("Servidor em execução: https://la-casa-de-pastel-api.vercel.app"))
